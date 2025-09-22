bd0fb0b941c88736d56065aa5d4438f0f0df7658
sanjanayadav07 <sanjanayadav3952@gmail.com>
2025-09-22 15:57:50 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -52,4 +52,4 @@ app.use("/api/messages", messageRouter);
 await connectDB();
 
 const PORT = process.env.PORT || 5000
-server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT))
+//server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT))
